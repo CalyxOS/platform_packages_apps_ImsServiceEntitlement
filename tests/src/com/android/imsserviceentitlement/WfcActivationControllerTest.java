4e651d334b2f32e0919110ad6a76bd8ca424467d
danielwbhuang <danielwbhuang@google.com>
2021-01-19 00:05:12 +0800

    Add WfcActivationControllerTest for ServiceEntitment
    Add WfcActivationControllerTest.
    
    Bug: 176127289
    Test: atest ImsServiceEntitlementTests
    com.android.imsserviceentitlement.tests (4 Tests)
    [1/4] com.android.imsserviceentitlement.WfcActivationControllerTest#startFlow_launchAppForActivation_setPurposeActivation: PASSED (155ms)
    [2/4] com.android.imsserviceentitlement.WfcActivationControllerTest#startFlow_launchAppForShowTc_setPurposeUpdate: PASSED (2ms)
    [3/4] com.android.imsserviceentitlement.WfcActivationControllerTest#finishFlow_isFinishing_showGeneralWaitingUi: PASSED (26ms)
    [4/4] com.android.imsserviceentitlement.WfcActivationControllerTest#startFlow_launchAppForUpdate_setPurposeUpdate: PASSED (26ms)
    
    Change-Id: I599c4169157e030b8937dbf7abd13128a5d9e4e8

--- tests/src/com/android/imsserviceentitlement/WfcActivationControllerTest.java
+++ tests/src/com/android/imsserviceentitlement/WfcActivationControllerTest.java
@@ -16,18 +16,157 @@
 
 package com.android.imsserviceentitlement;
 
-import static com.google.common.truth.Truth.assertThat;
+import static org.mockito.Mockito.inOrder;
+import static org.mockito.Mockito.spy;
+import static org.mockito.Mockito.when;
 
+import android.app.Activity;
+import android.app.Instrumentation;
+import android.content.Context;
+import android.content.Intent;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
+import android.telephony.SubscriptionManager;
+import android.telephony.TelephonyManager;
+
+import androidx.test.core.app.ApplicationProvider;
 import androidx.test.runner.AndroidJUnit4;
 
+import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.mockito.InOrder;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+
+import java.lang.reflect.Field;
+
+
 
 // TODO(b/176127289) add tests
 @RunWith(AndroidJUnit4.class)
 public class WfcActivationControllerTest {
+    @Mock private TelephonyManager mTelephonyManager;
+    @Mock private WfcActivationApi mActivationApi;
+    @Mock private WfcActivationUi mActivationUi;
+    @Mock private ConnectivityManager mConnectivityManager;
+    @Mock private NetworkInfo mNetworkInfo;
+
+    private static final String WEBVIEW_JS_CONTROLLER_NAME = "webviewJsControllerName";
+    private static final int SUB_ID = 1;
+
+    private WfcActivationController mWfcActivationController;
+    private Context mContext;
+    private Instrumentation mInstrumentation;
+
+    @Before
+    public void setUp() throws Exception {
+        MockitoAnnotations.initMocks(this);
+        mContext = spy(ApplicationProvider.getApplicationContext());
+
+        when(mActivationApi.getWebviewJsControllerName()).thenReturn(WEBVIEW_JS_CONTROLLER_NAME);
+        when(mContext.getSystemService(TelephonyManager.class)).thenReturn(mTelephonyManager);
+        when(mTelephonyManager.createForSubscriptionId(SUB_ID)).thenReturn(mTelephonyManager);
+        setNetworkConnected(true);
+
+        // now do not need to try/catch
+        Field field = EntitlementUtils.class.getDeclaredField("useDirectExecutorForTest");
+        field.setAccessible(true);
+        field.set(null, true);
+    }
+
     @Test
-    public void test() {
-        assertThat(1).isEqualTo(1);
+    public void startFlow_launchAppForActivation_setPurposeActivation() {
+        InOrder mOrderVerifier = inOrder(mActivationUi);
+        setNetworkConnected(false);
+        buildActivity(ActivityConstants.LAUNCH_APP_ACTIVATE);
+
+        mWfcActivationController.startFlow();
+
+        verifyGeneralWaitingUi(mOrderVerifier, R.string.activate_title);
+        verifyErrorUi(mOrderVerifier, R.string.activate_title, R.string.wfc_activation_error);
+    }
+
+    @Test
+    public void startFlow_launchAppForUpdate_setPurposeUpdate() {
+        InOrder mOrderVerifier = inOrder(mActivationUi);
+        setNetworkConnected(false);
+        buildActivity(ActivityConstants.LAUNCH_APP_UPDATE);
+
+        mWfcActivationController.startFlow();
+
+        verifyGeneralWaitingUi(mOrderVerifier, R.string.e911_title);
+        verifyErrorUi(mOrderVerifier, R.string.e911_title, R.string.address_update_error);
+    }
+
+    @Test
+    public void startFlow_launchAppForShowTc_setPurposeUpdate() {
+        InOrder mOrderVerifier = inOrder(mActivationUi);
+        setNetworkConnected(false);
+        buildActivity(ActivityConstants.LAUNCH_APP_SHOW_TC);
+
+        mWfcActivationController.startFlow();
+
+        verifyGeneralWaitingUi(mOrderVerifier, R.string.tos_title);
+        verifyErrorUi(mOrderVerifier, R.string.tos_title, R.string.show_terms_and_condition_error);
+    }
+
+    @Test
+    public void finishFlow_isFinishing_showGeneralWaitingUi() {
+        InOrder mOrderVerifier = inOrder(mActivationUi);
+        when(mActivationApi.checkEntitlementStatus()).thenReturn(null);
+        buildActivity(ActivityConstants.LAUNCH_APP_ACTIVATE);
+
+        mWfcActivationController.finishFlow();
+
+        mOrderVerifier
+                .verify(mActivationUi)
+                .showActivationUi(
+                        R.string.activate_title,
+                        R.string.progress_text,
+                        true,
+                        0,
+                        Activity.RESULT_CANCELED,
+                        0);
+        mOrderVerifier
+                .verify(mActivationUi)
+                .showActivationUi(
+                        R.string.activate_title,
+                        R.string.wfc_activation_error,
+                        false,
+                        R.string.ok,
+                        WfcActivationUi.RESULT_FAILURE,
+                        0);
+    }
+
+    private void buildActivity(int extraLaunchCarrierApp) {
+        Intent intent = new Intent(Intent.ACTION_MAIN);
+        intent.putExtra(SubscriptionManager.EXTRA_SUBSCRIPTION_INDEX, SUB_ID);
+        intent.putExtra(ActivityConstants.EXTRA_LAUNCH_CARRIER_APP, extraLaunchCarrierApp);
+        mWfcActivationController =
+                new WfcActivationController(mContext, mActivationUi, mActivationApi, intent);
+    }
+
+    private void setNetworkConnected(boolean isConnected) {
+        when(mNetworkInfo.isConnected()).thenReturn(isConnected);
+        when(mContext.getSystemService(Context.CONNECTIVITY_SERVICE)).thenReturn(
+                mConnectivityManager);
+        when(mConnectivityManager.getActiveNetworkInfo()).thenReturn(mNetworkInfo);
+        when(mNetworkInfo.isConnected()).thenReturn(isConnected);
+    }
+
+    private void verifyErrorUi(InOrder inOrder, int title, int errorMesssage) {
+        inOrder.verify(mActivationUi)
+                .showActivationUi(
+                        title,
+                        errorMesssage,
+                        false, R.string.ok,
+                        WfcActivationUi.RESULT_FAILURE,
+                        0);
+    }
+
+    private void verifyGeneralWaitingUi(InOrder inOrder, int title) {
+        inOrder.verify(mActivationUi)
+                .showActivationUi(title, R.string.progress_text, true, 0, 0, 0);
     }
 }
