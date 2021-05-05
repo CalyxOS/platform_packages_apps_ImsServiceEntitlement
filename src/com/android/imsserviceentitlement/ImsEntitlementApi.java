14cb386ad7a6f517029fb6de01093ed9a1d63ce8
samalin <samalin@google.com>
2021-05-05 17:58:45 +0800

    Implement the data store for saving the characteristics
    Entitlement result will be store as raw xml doc in shared preference
    
    Bug: 187254979
    Test: atest ImsServiceEntitlementUnitTests
    Change-Id: I4cbfc679d78f60799ae857392bc13b944567906b

---
+++ src/com/android/imsserviceentitlement/ImsEntitlementApi.java
@@ -0,0 +1,134 @@
+/*
+ * Copyright (C) 2021 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.android.imsserviceentitlement;
+
+import android.content.Context;
+import android.text.TextUtils;
+import android.util.Log;
+
+import androidx.annotation.Nullable;
+import androidx.annotation.VisibleForTesting;
+
+import com.android.imsserviceentitlement.entitlement.EntitlementConfiguration;
+import com.android.imsserviceentitlement.entitlement.EntitlementResult;
+import com.android.imsserviceentitlement.fcm.FcmTokenStore;
+import com.android.imsserviceentitlement.fcm.FcmUtils;
+import com.android.imsserviceentitlement.ts43.Ts43Constants.ResponseXmlAttributes;
+import com.android.imsserviceentitlement.ts43.Ts43Constants.ResponseXmlNode;
+import com.android.imsserviceentitlement.ts43.Ts43VowifiStatus;
+import com.android.imsserviceentitlement.utils.TelephonyUtils;
+import com.android.imsserviceentitlement.utils.XmlDoc;
+import com.android.libraries.entitlement.CarrierConfig;
+import com.android.libraries.entitlement.ServiceEntitlement;
+import com.android.libraries.entitlement.ServiceEntitlementException;
+import com.android.libraries.entitlement.ServiceEntitlementRequest;
+
+import com.google.common.collect.ImmutableList;
+
+/** Implementation of the entitlement API. */
+public class ImsEntitlementApi {
+    private static final String TAG = "IMSSE-ImsEntitlementApi";
+
+    private static final String JS_CONTROLLER_NAME = "VoWiFiWebServiceFlow";
+
+    private final Context mContext;
+    private final int mSubId;
+    private final ServiceEntitlement mServiceEntitlement;
+    private final EntitlementConfiguration mLastEntitlementConfiguration;
+
+    private String mCachedAccessToken;
+
+    public ImsEntitlementApi(Context context, int subId) {
+        this.mContext = context;
+        this.mSubId = subId;
+        CarrierConfig carrierConfig = getCarrierConfig(context);
+        this.mServiceEntitlement = new ServiceEntitlement(context, carrierConfig, subId);
+        this.mLastEntitlementConfiguration = new EntitlementConfiguration(context, subId);
+    }
+
+    @VisibleForTesting
+    ImsEntitlementApi(Context context, int subId, ServiceEntitlement serviceEntitlement,
+            EntitlementConfiguration lastEntitlementConfiguration) {
+        this.mContext = context;
+        this.mSubId = subId;
+        this.mServiceEntitlement = serviceEntitlement;
+        this.mLastEntitlementConfiguration = lastEntitlementConfiguration;
+    }
+
+    /**
+     * Returns WFC entitlement check result from carrier API (over network), or {@code null} on
+     * unrecoverable network issue or malformed server response. This is blocking call so should not
+     * be called on main thread.
+     */
+    @Nullable
+    public EntitlementResult checkEntitlementStatus() {
+        Log.d(TAG, "checkEntitlementStatus subId=" + mSubId);
+
+        ServiceEntitlementRequest.Builder requestBuilder = ServiceEntitlementRequest.builder();
+        if (!TextUtils.isEmpty(mCachedAccessToken)) {
+            requestBuilder.setAuthenticationToken(mCachedAccessToken);
+        }
+        FcmUtils.fetchFcmToken(mContext, mSubId);
+        requestBuilder.setNotificationToken(FcmTokenStore.getToken(mContext, mSubId));
+        // Set fake device info to avoid leaking
+        requestBuilder.setTerminalVendor("vendorX");
+        requestBuilder.setTerminalModel("modelY");
+        requestBuilder.setTerminalSoftwareVersion("versionZ");
+        ServiceEntitlementRequest request = requestBuilder.build();
+
+        XmlDoc entitlementXmlDoc = null;
+        try {
+            String rawXml = mServiceEntitlement.queryEntitlementStatus(
+                    ImmutableList.of(ServiceEntitlement.APP_VOWIFI), request);
+            entitlementXmlDoc = new XmlDoc(rawXml);
+            // While finishing the initial AuthN, save the configs from the result doc
+            mLastEntitlementConfiguration.update(rawXml);
+            mLastEntitlementConfiguration.getToken().ifPresent(token -> mCachedAccessToken = token);
+        } catch (ServiceEntitlementException e) {
+            Log.e(TAG, "queryEntitlementStatus failed", e);
+        }
+        return entitlementXmlDoc == null ? null : toEntitlementResult(entitlementXmlDoc);
+    }
+
+    private static EntitlementResult toEntitlementResult(XmlDoc doc) {
+        EntitlementResult.Builder builder =
+                EntitlementResult.builder()
+                        .setSuccess(true)
+                        .setVowifiStatus(Ts43VowifiStatus.builder(doc).build());
+        doc.get(
+                ResponseXmlNode.APPLICATION,
+                ResponseXmlAttributes.SERVER_FLOW_URL,
+                ServiceEntitlement.APP_VOWIFI)
+                .ifPresent(url -> builder.setEmergencyAddressWebUrl(url));
+        doc.get(
+                ResponseXmlNode.APPLICATION,
+                ResponseXmlAttributes.SERVER_FLOW_USER_DATA,
+                ServiceEntitlement.APP_VOWIFI)
+                .ifPresent(userData -> builder.setEmergencyAddressWebData(userData));
+        return builder.build();
+    }
+
+    private CarrierConfig getCarrierConfig(Context context) {
+        String entitlementServiceUrl = TelephonyUtils.getEntitlementServerUrl(context, mSubId);
+        return CarrierConfig.builder().setServerUrl(entitlementServiceUrl).build();
+    }
+
+    /** Returns the name of JS controller object used in emergency address webview. */
+    public String getWebviewJsControllerName() {
+        return JS_CONTROLLER_NAME;
+    }
+}
---
+++ src/com/android/imsserviceentitlement/entitlement/EntitlementConfiguration.java
@@ -0,0 +1,183 @@
+/*
+ * Copyright (C) 2021 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.android.imsserviceentitlement.entitlement;
+
+import android.content.Context;
+
+import com.android.imsserviceentitlement.ts43.Ts43Constants.ResponseXmlAttributes;
+import com.android.imsserviceentitlement.ts43.Ts43Constants.ResponseXmlNode;
+import com.android.imsserviceentitlement.utils.XmlDoc;
+import com.android.libraries.entitlement.ServiceEntitlement;
+
+import java.util.Optional;
+
+/** Provides the entitlement characteristic which stored from previous query. */
+public class EntitlementConfiguration {
+    /** Default value of version for VERS characteristic. */
+    private static final int DEFAULT_VERSION = 0;
+    /** Default value of validity for VERS and TOKEN characteristics. */
+    private static final int DEFAULT_VALIDITY = 0;
+    /** Default value of VoLTE/VoWifi/SMSoverIP entitlemenet status. */
+    private static final int INCOMPATIBLE_STATE = 2;
+
+    private final EntitlementConfigurationsDataStore mConfigutationsDataStore;
+
+    private XmlDoc mXmlDoc = new XmlDoc(null);
+
+    public EntitlementConfiguration(Context context, int subId) {
+        mConfigutationsDataStore = new EntitlementConfigurationsDataStore(context, subId);
+        mConfigutationsDataStore.get().ifPresent(rawXml -> mXmlDoc = new XmlDoc(rawXml));
+    }
+
+    /** Update VERS characteristics with given version and validity. */
+    public void update(String rawXml) {
+        mConfigutationsDataStore.set(rawXml);
+        mXmlDoc = new XmlDoc(rawXml);
+    }
+
+    /**
+     * Returns VoLTE entitlement status from the {@link EntitlementCharacteristicDataStore}. If no
+     * data exist then return the default value {@link #INCOMPATIBLE_STATE}.
+     */
+    public int getVolteStatus() {
+        return mXmlDoc.get(
+                ResponseXmlNode.APPLICATION,
+                ResponseXmlAttributes.ENTITLEMENT_STATUS,
+                ServiceEntitlement.APP_VOLTE)
+                .map(Integer::parseInt)
+                .orElse(INCOMPATIBLE_STATE);
+    }
+
+    /**
+     * Returns VoWiFi entitlement status from the {@link EntitlementCharacteristicDataStore}. If no
+     * data exist then return the default value {@link #INCOMPATIBLE_STATE}.
+     */
+    public int getVoWifiStatus() {
+        return mXmlDoc.get(
+                ResponseXmlNode.APPLICATION,
+                ResponseXmlAttributes.ENTITLEMENT_STATUS,
+                ServiceEntitlement.APP_VOWIFI)
+                .map(Integer::parseInt)
+                .orElse(INCOMPATIBLE_STATE);
+    }
+
+    /**
+     * Returns SMSoIP entitlement status from the {@link EntitlementCharacteristicDataStore}. If no
+     * data exist then return the default value {@link #INCOMPATIBLE_STATE}.
+     */
+    public int getSmsOverIpStatus() {
+        return mXmlDoc.get(
+                ResponseXmlNode.APPLICATION,
+                ResponseXmlAttributes.ENTITLEMENT_STATUS,
+                ServiceEntitlement.APP_SMSOIP)
+                .map(Integer::parseInt)
+                .orElse(INCOMPATIBLE_STATE);
+    }
+
+    /** Returns token stored in the {@link EntitlementCharacteristicDataStore}. */
+    public Optional<String> getToken() {
+        return mXmlDoc.get(ResponseXmlNode.TOKEN, ResponseXmlAttributes.TOKEN, null);
+    }
+
+    /**
+     * Returns the Validity of the token expiry duration. Validity is defined in seconds from the
+     * time the Entitlement Client receives it. If no data exist then returns default value 0.
+     */
+    public int getTokenValidity() {
+        return mXmlDoc.get(
+                ResponseXmlNode.TOKEN,
+                ResponseXmlAttributes.VALIDITY,
+                null)
+                .map(Integer::parseInt)
+                .orElse(DEFAULT_VALIDITY);
+    }
+
+    public enum ClientBehavior {
+        /** Unknown behavior. */
+        UNKNOWN_BEHAVIOR,
+        /** Entitlement data is valid during validity seconds. */
+        VALID_DURING_VALIDITY,
+        /** Entitlement data is valid without any limitation of duration. */
+        VALID_WITHOUT_DURATION,
+        /** Entitlement data has to be reset to default configuration */
+        NEEDS_TO_RESET,
+        /**
+         * Entitlement data has to be reset to default configuration except for version and
+         * validity. The Entitlement Client shall not perform client configuration requests anymore
+         * for the services just configured.
+         */
+        NEEDS_TO_RESET_EXCEPT_VERS,
+        /**
+         * entitlement data has to be reset to default configuration except for version and
+         * validity. The Entitlement Client shall not perform client configuration requests anymore
+         * for the services just configured, until the end-user switches the setting to On.
+         */
+        NEEDS_TO_RESET_EXCEPT_VERS_UNTIL_SETTING_ON,
+    }
+
+    /** Returns {@link ClientBehavior} for the service to be configured. */
+    public ClientBehavior entitlementValidation() {
+        int version = mXmlDoc.get(
+                ResponseXmlNode.VERS,
+                ResponseXmlAttributes.VERSION,
+                null)
+                .map(Integer::parseInt)
+                .orElse(DEFAULT_VERSION);
+        int validity = mXmlDoc.get(
+                ResponseXmlNode.VERS,
+                ResponseXmlAttributes.VALIDITY,
+                null)
+                .map(Integer::parseInt)
+                .orElse(DEFAULT_VALIDITY);
+
+        if (version > 0 && validity > 0) {
+            return ClientBehavior.VALID_DURING_VALIDITY;
+        } else if (version > 0 && validity == 0) {
+            return ClientBehavior.VALID_WITHOUT_DURATION;
+        } else if (version == 0 && validity == 0) {
+            return ClientBehavior.NEEDS_TO_RESET;
+        } else if (version == -1 && validity == -1) {
+            return ClientBehavior.NEEDS_TO_RESET_EXCEPT_VERS;
+        } else if (version == -2 && validity == -2) {
+            return ClientBehavior.NEEDS_TO_RESET_EXCEPT_VERS_UNTIL_SETTING_ON;
+        }
+
+        // Should never reach.
+        return ClientBehavior.UNKNOWN_BEHAVIOR;
+    }
+
+    /**
+     * Removes the stored configuration and reverts to the default configuration when:
+     * <ul>
+     *   <li>on SIM card change
+     *   <li>on menu Factory reset
+     *   <li>if a service is barred by the Service Provider (i.e. configuration version set to 0,
+     *       -1, -2). In that case, version and validity are not reset
+     * </ul>
+     */
+    public void reset() {
+        // Default configuration of the Characteristics:
+        //   - VERS.version = 0
+        //   - VERS.validity = 0
+        //   - TOKEN.token = null (i.e. no value assigned)
+        //   - TOKEN.validity =0
+        //   - VoLTE.EntitlementStatus=2 (INCOMPATIBLE_STATE)
+        //   - VoWiFi.EntitlementStatus=2 (INCOMPATIBLE_STATE)
+        //   - SMSoIP.EntitlementStatus=2 (INCOMPATIBLE_STATE)
+        update(null);
+    }
+}
